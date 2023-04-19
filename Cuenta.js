9ed0f2c71a942795ea344286d51f444305c950d2
ivanchino <acevedoamaroivan@gmail.com>
2023-04-19 00:26:34 -0600

    En pausa se seguira mañana

--- Cuenta.js
+++ Cuenta.js
@@ -2,12 +2,20 @@ export class Cuenta{
     #cliente;
     #saldo;
 
-    constructor(tipo, cliente, numero, agencia, saldo){
-        this.tipo = tipo;
+    constructor(cliente, numero, agencia, saldo){
         this.numero = numero;
         this.agencia = agencia;
         this.#cliente = cliente;
         this.#saldo = saldo;
+    }
+
+    set cliente(valor) {
+        if (valor instanceof Cliente)
+            this.#cliente = valor;
+    }
+
+    get cliente() {
+        return this.#cliente;
     }
 
     depositoEnCuenta(valor) {
@@ -17,10 +25,6 @@ export class Cuenta{
     }
 
     retirarDeCuenta(valor) {
-        if (this.tipo == 'Corriente')
-            valor = valor * 1.05;
-        else if (this.tipo == 'Ahorro')
-            valor = valor * 1.02;
         if (valor <= this.#saldo)
             this.#saldo -= valor;
         return this.#saldo;
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
-// import {CuentaCorriente} from './CuentaCorriente.js';
-// import {CuentaAhorro} from './CuentaAhorro.js';
+import {CuentaCorriente} from './CuentaCorriente.js';
+import {CuentaAhorro} from './CuentaAhorro.js';
 import { Cuenta } from './Cuenta.js';
 
 const cliente = new Cliente('Leonardo','13804050','123224');
 const cliente2 = new Cliente('María','16979808','8989');
 
-const cuentaDeLeonardo = new Cuenta('Corriente', cliente, '1', '001', 0);
-const cuentaDeMaria = new Cuenta('Corriente', cliente2,'2','002', 0);
+const cuentaDeLeonardo = new CuentaCorriente('Corriente', cliente, '1', '001');
+const cuentaDeMaria = new CuentaCorriente('Corriente', cliente2,'2','002');
 
-const cuentaAhorroLeonardo = new Cuenta('Ahorro', cliente, '9985', '001', 0);
+const cuentaAhorroLeonardo = new CuentaAhorro('Ahorro', cliente, '9985', '001', 0);
 
 console.log(cuentaDeLeonardo);
 cuentaDeLeonardo.depositoEnCuenta(150);
